b38f2ad61d64e2600b2c7aa159e7871618885981
serena <serenagandhi1@aol.com>
2020-06-26 17:56:39 -0500

    author search

--- getPoem.js
+++ getPoem.js
@@ -98,6 +98,52 @@ function findPoem() {
         });     
     } else if (inputTitle === "") {
         //display random poet by the provided author, if possible
+        var link = "https://poetrydb.org/author,poemcount/" + inputPoet + ";10000";
+        fetch(link).then(r => r.text()).then(result => {
+            //Convert result to JSON object array to check for errors
+            var poems = '{ "poems" : ['+ result + ']}';
+            var poem = JSON.parse(poems);
+            //check to see if result has an error
+            if (poem.poems.length < 3){
+                document.getElementById("searchMessage").innerHTML = "Please try again.";
+            }
+            var poems = '{ "poems" : '+ result + '}';
+            var poem = JSON.parse(poems);
+            var num = Math.random() * poem.poems.length;
+            var intNum = ~~num;
+            //find title, lines, and name
+            var title = poem.poems[intNum].title.toString();
+            var name = poem.poems[intNum].author.toString();
+            var lines = poem.poems[intNum].lines.toString();
+           //parse code to fit normal formatting standards
+            lines = lines.replace(/,  /g, "COMMASPACESPACE");
+            lines = lines.replace(/, /g, "COMMASPACE");
+            lines = lines.replace(/,,/g , ",<br>");
+            lines = lines.replace(/,/g, "<br>");
+            lines = lines.replace(/COMMASPACESPACE/g, "<br> &nbsp &nbsp &nbsp");
+            lines = lines.replace(/COMMASPACE/g, ", ");
+            lines = lines.replace(/<br><br>/g, "<br>")
+            lines = lines.replace(/<br>-/g, "-");
+            lines = lines.replace(/<br>' /g, "'");
+            lines = lines.replace(/<br>'<br>/g, "'<br>");
+            //display poem's title, author, and lines
+            document.getElementById("title").innerHTML = title;
+            document.getElementById("author").innerHTML = name;
+            document.getElementById("lines").innerHTML = lines;
+            //create link using author's name
+            var urlName = name.toLowerCase();
+            urlName = urlName.replace(/ /g, "-");
+            var link = "https://www.poemist.com/" + urlName;
+            function getLink() {
+            document.getElementById("authorLink").href = link;
+            }
+            getLink(); 
+            document.getElementById("searchMessage").innerHTML = "";
+        },
+        error => {
+           var poem = "Could not find a poem."
+           document.getElementById("title").innerHTML = poem;
+        });  
     } else {
         //use both to display specific poem, if possible
     }
